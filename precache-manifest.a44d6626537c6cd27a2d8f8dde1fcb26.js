053c9e92e699b00424b7326c9f3a1a3d06a5dead
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-05-20 23:37:12 +0530

    Updates

---
+++ precache-manifest.a44d6626537c6cd27a2d8f8dde1fcb26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc5864007a549df5625e2597d4e3fcd",
+    "url": "/SampleReact/index.html"
+  },
+  {
+    "revision": "ee3063a0648659c4a26f",
+    "url": "/SampleReact/static/css/main.5f18896b.chunk.css"
+  },
+  {
+    "revision": "a5a96e4b5024f9cedd53",
+    "url": "/SampleReact/static/js/2.c84aa9c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SampleReact/static/js/2.c84aa9c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee3063a0648659c4a26f",
+    "url": "/SampleReact/static/js/main.8bd70cb9.chunk.js"
+  },
+  {
+    "revision": "493972eeac6eb293e488",
+    "url": "/SampleReact/static/js/runtime-main.4727ca6d.js"
+  }
+]);
